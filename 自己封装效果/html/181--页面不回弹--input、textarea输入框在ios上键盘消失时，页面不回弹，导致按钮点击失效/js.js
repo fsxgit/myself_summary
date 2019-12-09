e5780a16961ebc51a525ddadd4ff7c02d1736070
jinye <jinye@epaybk.com>
2019-12-09 17:40:25 +0800

    新增富文本编辑器

--- 自己封装效果/html/181--页面不回弹--input、textarea输入框在ios上键盘消失时，页面不回弹，导致按钮点击失效/js.js
+++ 自己封装效果/html/181--页面不回弹--input、textarea输入框在ios上键盘消失时，页面不回弹，导致按钮点击失效/js.js
@@ -2,8 +2,9 @@
  * 解决input、textarea在ios中获取焦点后，页面被顶上，失去焦点时，页面不回弹，导致页面其他按钮点击不起作用
  */
 window.addEventListener('focusout', function () {
+  // document.body.scrollTop = document.documentElement.scrollTop;
   document.body.scrollTop = document.body.scrollHeight;
-  window.scroll(0, 0);
+  // window.scroll(0, 0);
 });
 
 
@@ -13,9 +14,7 @@ window.addEventListener('focusout', function () {
  */
 $("body").on("focus","input,textarea",function(){
   $(".calc_teach_share").hide();
-  $(".c_bot_gap").hide();
 });
 $("body").on("blur","input,textarea",function(){
   $(".calc_teach_share").show();
-  $(".c_bot_gap").show();
 });
